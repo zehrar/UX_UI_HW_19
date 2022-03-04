6ab4e28b3221a72d00a9f3a281e9a730a3e110e6
zehrar <itszehrariz@gmail.com>
2022-03-04 18:54:48 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// script for index
+
+$(document).ready(function() {
+
+    console.log("in doc ready");
+    // li is the element that is being selected to use the hover function on
+    $("li").hover(function(){
+        // this is the color when you are hovering over it
+        $(this).css("background-color", "pink")
+    }, function(){
+        // this is the color after you move your mouse (stopped hovering)
+        $(this).css("background-color", "white")
+    })
+    
+  });
